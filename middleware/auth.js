c4679f4a485e7d59f401d4c8801ae834d2644c97
dani.edo <dani.nurafandi@gomodo.tech>
2020-03-17 23:07:48 +0700

    success upload file to storage

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,6 @@ import 'firebase/auth'
 
 export default ({ redirect }) => {
   firebase.auth().onAuthStateChanged((user) => {
-    console.log('user => ', user)
     if (!user) {
       return redirect('/auth/login')
     }
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,26 +1,5 @@
 <template>
   <div>
-    <!-- <div class="container">
-      <div>
-        <b-row>
-          <b-col
-            v-for="(item, index) in cards"
-            :key="index"
-            md="6"
-            class="mb-3"
-          >
-            <Card
-              :href-link="item.hrefLink"
-              :title="item.cardTitle"
-              :description="item.cardDescription"
-              :image-link="item.imageLink"
-              :video-link="item.videoLink"
-              :card-link="true"
-            />
-          </b-col>
-        </b-row>
-      </div>
-    </div> -->
     <v-app id="inspire">
       <Navbar />
 
@@ -29,22 +8,34 @@
           <v-row align="center" justify="center">
             <v-col class="shrink">
               <v-card class="mx-auto" max-width="344">
-                <v-img
-                  src="https://images.unsplash.com/photo-1513398886898-6ae5ff7820f3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjkwNjAxfQ"
-                  height="200px"
-                ></v-img>
+                <v-img :src="cardImage" height="200px"></v-img>
 
-                <v-card-title>
-                  Upload Video
-                </v-card-title>
+                <!-- <v-card-title>
+                  Upload something here
+                </v-card-title> -->
 
                 <v-card-subtitle>
-                  1,000 miles of wonder
+                  Upload something here
                 </v-card-subtitle>
 
-                <v-card-actions>
+                <v-card-actions class="d-block">
+                  <v-progress-linear
+                    v-model="uploadProgress"
+                    height="25"
+                    reactive
+                  >
+                    <strong>{{ Math.ceil(uploadProgress) }}%</strong>
+                  </v-progress-linear>
+                  <v-file-input
+                    label="input file"
+                    class="w-100"
+                    @change="onFilePicked"
+                  ></v-file-input>
                   <v-btn color="purple" class="w-100" text>
                     Upload
+                  </v-btn>
+                  <v-btn color="green" class="w-100" text @click="downloadFile">
+                    Download
                   </v-btn>
                 </v-card-actions>
               </v-card>
@@ -57,10 +48,30 @@
         <div class="ml-auto">&copy; by Tutodiv 2020</div>
       </v-footer>
     </v-app>
+    <v-dialog v-model="shownModal" max-width="290">
+      <v-card>
+        <v-card-title class="headline">Cheers!</v-card-title>
+
+        <v-card-text>
+          Upload Success
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="green darken-1" text @click="shownModal = false">
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+
 import Navbar from '~/components/Navbar'
 // import Card from '~/components/Card'
 
@@ -78,31 +89,77 @@ export default {
   },
   data() {
     return {
-      cards: [
-        {
-          cardTitle: 'How to make a coffee',
-          cardDescription:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci excepturi libero autem aut, tempora facere nihil, obcaecati deserunt laboriosam, reprehenderit dolore aperiam ipsam quis consequuntur reiciendis fugit voluptate veritatis. Labore.',
-          hrefLink: '/dashboard/detail',
-          imageLink:
-            'https://images.unsplash.com/photo-1510279931157-4ca63af8a363?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjkwNjAxfQ',
-          videoLink: 'RFcuNI33Oq4'
-        },
-        {
-          cardTitle: 'How to make a tea',
-          cardDescription:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci excepturi libero autem aut, tempora facere nihil, obcaecati deserunt laboriosam, reprehenderit dolore aperiam ipsam quis consequuntur reiciendis fugit voluptate veritatis. Labore.',
-          hrefLink: '/dashboard/detail',
-          imageLink:
-            'https://images.unsplash.com/photo-1558160074-4d7d8bdf4256?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjkwNjAxfQ',
-          videoLink: 'le6vodQGeVI'
-        }
-      ],
-      show: false
+      image: null,
+      cardImage:
+        'https://images.unsplash.com/photo-1513398886898-6ae5ff7820f3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjkwNjAxfQ',
+      uploadProgress: 0,
+      shownModal: false
+    }
+  },
+  watch: {
+    shownModal() {
+      console.log('executed')
+      if (this.shownModal === false) {
+        location.reload()
+      }
     }
   },
   created() {
     this.$vuetify.theme.dark = true
+  },
+  methods: {
+    onFilePicked(e) {
+      if (e) {
+        console.log(e, 'run change!')
+
+        // Get File
+        const filename = e.name
+        if (filename.lastIndexOf('.') <= 0) {
+          return alert('Please add a valid file!')
+        }
+        // const fileReader = new FileReader()
+        // fileReader.addEventListener('load', () => {
+        //   this.cardImage = fileReader.result
+        // })
+        // fileReader.readAsDataURL(e)
+        // this.image = e
+
+        // Create a storage ref
+        const storageRef = firebase.storage().ref('photos/' + filename)
+
+        // Upload file
+        const task = storageRef.put(e)
+
+        // Update progressbar
+        task.on(
+          'state_changed',
+          (snapshot) => {
+            const percentage =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            this.uploadProgress = percentage
+            console.log('uploadProgress', this.uploadProgress)
+          },
+          (error) => {
+            console.log(error)
+          },
+          () => {
+            console.log('complete')
+            this.shownModal = true
+          }
+        )
+      } else {
+        console.log('file undefined')
+      }
+    },
+    downloadFile() {
+      firebase
+        .storage()
+        .ref('supercell - Sayonara Memories.mp4')
+        .getDownloadURL()
+        .then((data) => {
+          console.log(data)
+        })
+    }
   }
 }
 </script>
